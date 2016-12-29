525dae0808b67c40bf9dd5ca1477fdfd922977f6
Proffsolver <serebrennikov.evgeniy@gmail.com>
2016-12-29 22:32:08 +0300

    Сделал аутинтификацию, прокинул вьюхолдер, сделал преференс менеджер. Рест сервис и еще много чего.

---
+++ app/src/main/java/mobi/gpsmarker/gpsmarkercommander/data/network/req/SettingsDeviceOption.java
@@ -0,0 +1,16 @@
+package mobi.gpsmarker.gpsmarkercommander.data.network.req;
+
+public class SettingsDeviceOption {
+
+    public String id_user;
+    public String token;
+    public String id_device;
+    private SettingsDeviceData data;
+
+    public SettingsDeviceOption(String id_user, String token, String id_device, SettingsDeviceData data) {
+        this.id_user = id_user;
+        this.token = token;
+        this.id_device = id_device;
+        this.data = data;
+    }
+}
